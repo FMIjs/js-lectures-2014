d38fbb2fd67967ba002517fa62510a5308fa2f9e
Evgeni Kunev <evgeni@kunev.me>
2014-08-31 18:58:14 +0300

    Initial commit

---
+++ compile.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+var hogan = require('hogan'),
+    fs = require('fs'),
+    path = require('path'),
+    Q = require('q'),
+    lecturesDir = './lectures',
+    compiledDir = './compiled',
+    layoutFilename = './layout.html.mustache',
+    requiredForComiple = process.argv.slice(2);
+
+
+function discoverLectures() {
+  var deferred = Q.defer();
+  fs.readdir(lecturesDir, function (error, lectures) {
+    if (error) {
+      deferred.reject(new Error(error));
+    } else {
+      deferred.resolve(lectures);
+    }
+  });
+
+  return deferred.promise;
+}
+
+function loadLayout() {
+  var deferred = Q.defer();
+  fs.readFile(layoutFilename, function (error, data) {
+    if (error) {
+      console.error('Too bad: ' + error);
+      deferred.reject(new Error(error));
+    } else {
+      deferred.resolve(hogan.compile(data.toString()));
+    }
+  });
+
+  return deferred.promise;
+}
+
+function compile(layout, template_name) {
+  var deferred = Q.defer();
+  fs.readFile(path.join(lecturesDir, template_name), function (error, data) {
+    if (error) {
+      deferred.reject(new Error(error));
+    } else {
+      deferred.resolve(layout.render({slides: data.toString()}));
+    }
+  });
+
+  return deferred.promise;
+}
+
+loadLayout().then(function (layout) {
+  discoverLectures().then(function (lectures) {
+    var templatePattern = process.argv[2],
+        templates = lectures.filter(function (lecture) {
+          return RegExp(templatePattern).test(lecture);
+        });
+
+    console.log(templates);
+
+    templates.forEach(function (template) {
+      compile(layout, template).then(function (output) {
+        var basename = template.split('.')[0],
+            compiled = path.join(compiledDir, basename);
+        fs.writeFile(compiled + '.html', output);
+      }, function (error) {
+        console.log('Could not read lecture file ' + template);
+        console.err(error);
+      });
+    });
+  });
+});
