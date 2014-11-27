9f33727a237e16d1acf4d72135a8b1c4d4c33f27
Stelf <larodi@gmail.com>
2014-11-27 12:48:57 +0200

    fix linter warnings

--- examples/05-futures/http.async.f.js
+++ examples/05-futures/http.async.f.js
@@ -2,16 +2,16 @@
 var q = require('q');
 var http = require('http');
 
-var cdest = { 
+var cdest = {
     hostname: 'www.abv.bg',
     path: '/',
-    port: 80 
+    port: 80
 };
 
 
 var pHTTPRequest = function() {
-    var defer = q.defer()
-      , args = Array.prototype.slice.call(arguments);
+    var defer = q.defer(),
+        args = Array.prototype.slice.call(arguments);
 
     args.push(function(res) {
       console.log('resolve request to URL');
@@ -22,7 +22,7 @@ var pHTTPRequest = function() {
 
     var req = http.request.apply(http, args);
 
-    req.on('error', function(e) { 
+    req.on('error', function(e) {
         console.log('resolve error ', e);
         defer.reject(e);
     });
@@ -39,18 +39,18 @@ var server = http.createServer(function(srvreq, srvres) {
     console.log('connection established');
 
     pHTTPRequest(cdest)
-        .then(function(getres) { 
+        .then(function(getres) {
             var data;
         
             console.log('req. to other server started');
         
-            getres.on('data', function(chunk) { 
+            getres.on('data', function(chunk) {
                 data = data + chunk;
-            })
+            });
 
             var dref = q.defer();
 
-            getres.on('end', function(chunk) { 
+            getres.on('end', function(chunk) {
                 console.log('req. to other server finished');
                 console.log('resolve with data length ', data.length);
                 dref.resolve(data);
@@ -58,7 +58,7 @@ var server = http.createServer(function(srvreq, srvres) {
 
             return dref.promise;
         })
-        .done(function (datares) { 
+        .done(function (datares) {
             console.log('write back data', datares.length);
             srvres.writeHead(200, {'Content-Type': 'text/html'});
             srvres.write(datares);
