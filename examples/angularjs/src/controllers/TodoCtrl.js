8c7cc44ce76674a2a5866eda0a6c02ba6cbd6f4d
mgechev <mgechev@gmail.com>
2015-01-14 18:59:18 +0200

    Add basic app and manifest

---
+++ examples/angularjs/src/controllers/TodoCtrl.js
@@ -0,0 +1,27 @@
+function TodoCtrl($scope) {
+  'use strict';
+
+  /*
+  {
+    "title": "Buy milk",
+    "completed": false|true
+  }
+  */
+
+  $scope.todos = JSON.parse(localStorage.getItem('todos') || '[]');
+  $scope.add = function () {
+    if ($scope.current) {
+      $scope.todos.push({
+        title: $scope.current,
+        completed: false
+      });
+      $scope.current = '';
+      localStorage.setItem('todos', JSON.stringify($scope.todos));
+    }
+  };
+
+  $scope.remove = function (index) {
+    $scope.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify($scope.todos));
+  };
+}
