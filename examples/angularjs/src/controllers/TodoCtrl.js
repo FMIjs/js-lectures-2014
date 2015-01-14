5ffb7b59ff7b247feac816ded7d2562d3dee7b2d
mgechev <mgechev@gmail.com>
2015-01-14 19:02:36 +0200

    Update controller declaration

--- examples/angularjs/src/controllers/TodoCtrl.js
+++ examples/angularjs/src/controllers/TodoCtrl.js
@@ -1,27 +1,28 @@
-function TodoCtrl($scope) {
-  'use strict';
+angular.module('todoApp').
+  controller('TodoCtrl', function TodoCtrl($scope) {
+    'use strict';
 
-  /*
-  {
-    "title": "Buy milk",
-    "completed": false|true
-  }
-  */
-
-  $scope.todos = JSON.parse(localStorage.getItem('todos') || '[]');
-  $scope.add = function () {
-    if ($scope.current) {
-      $scope.todos.push({
-        title: $scope.current,
-        completed: false
-      });
-      $scope.current = '';
-      localStorage.setItem('todos', JSON.stringify($scope.todos));
+    /*
+    {
+      "title": "Buy milk",
+      "completed": false|true
     }
-  };
+    */
 
-  $scope.remove = function (index) {
-    $scope.todos.splice(index, 1);
-    localStorage.setItem('todos', JSON.stringify($scope.todos));
-  };
-}
+    $scope.todos = JSON.parse(localStorage.getItem('todos') || '[]');
+    $scope.add = function () {
+      if ($scope.current) {
+        $scope.todos.push({
+          title: $scope.current,
+          completed: false
+        });
+        $scope.current = '';
+        localStorage.setItem('todos', JSON.stringify($scope.todos));
+      }
+    };
+
+    $scope.remove = function (index) {
+      $scope.todos.splice(index, 1);
+      localStorage.setItem('todos', JSON.stringify($scope.todos));
+    };
+  });
