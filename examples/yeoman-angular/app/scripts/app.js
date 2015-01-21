f41ad41cc00ad2b6a3cb16d90ffe5840ee9513e3
mgechev <mgechev@gmail.com>
2015-01-21 19:17:52 +0200

    Update files

--- examples/yeoman-angular/app/scripts/app.js
+++ examples/yeoman-angular/app/scripts/app.js
@@ -27,6 +27,10 @@ angular
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
       })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
